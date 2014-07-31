8d9db905152b085a22e2e3aff5fec106f62a9992
Alessandro <hello@alessndro.co.uk>
2014-07-31 22:30:51 +0100

    Fixed bug where UI would not display 0 seconds when Timer finished. Cleaned up a few typos and renamed some variables.

--- js/app.js
+++ js/app.js
@@ -12,15 +12,17 @@ Timer.prototype.start = function() {
 }
 
 Timer.prototype.tick = function() {
-  if (this.stopped != true && this.secsLeft > -1) {
-    
+  if (this.stopped != true && this.secsLeft > 0) {
+    this.secsLeft -= 1;
+
     if (this.timerAction != undefined) {
-      this.timerAction();  
+      this.timerAction();
     }
 
-    this.secsLeft -= 1;
     var that = this;
-    setTimeout(function(){that.tick();}, 1000);
+    setTimeout(function(){
+      that.tick();
+    }, 1000);
   } else {
     this.stopped = true;
   }
@@ -32,29 +34,32 @@ Timer.prototype.stop = function() {
 
 // Useful object to represent the view of the Timer
 var timerView = {
-  "startEl": $("#time-start"),
-  "endEl": $("#time-finish"),
+  // Hold references to the two stacked background divs
+  // Create the fading effect by gradually reducing opacity of the top one to 0
+  "startBg": $("#time-start"),
+  "endBg": $("#time-finish"),
   "currOpacity": 1,
   "resetUI": function() {
-    this.startEl.css("opacity", 1);
+    this.startBg.css("opacity", 1);
     this.currOpacity = 1;
   }
 };
 
-// Used as a callback for the Timer obejct that is executed every
-// time a second passes
+// Used as a callback when using the Timer object, which executes the callback 
+// every time a second passes
+// Creates the fading effect of the two background divs
 var updateUI = function(timer) {
   $("#the-time").text(secsToMins(timer.secsLeft));
   $("#time-start").css("opacity", timerView.currOpacity);
   timerView.currOpacity -= (1 / timer.duration);
 }
 
-// Creates and starts a new Timer with the corresponding duration according to the
+// Creates and starts a new Timer with the appropriate duration according to the
 // UI button clicked
 $("#buttons").on('click', 'p', function(e){
-  globalTimer = new Timer($(this).data('duration') * 60);
+  globalTimer = new Timer(parseInt($(this).data('duration') * 60));
   globalTimer.timerAction = function() { 
-    updateUI(globalTimer) 
+    updateUI(globalTimer);
   };
   timerView.resetUI();
   globalTimer.start();
